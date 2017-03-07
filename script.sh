0c1e31f6cfeccc09ca5820523ab264bd8b569c01
Jason Bennnefield <jasonbenne@gmail.com>
2017-03-07 13:42:28 -0600

    Fixed volume mount and sftp download dir

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sshpass -p "$WPE_PASSWORD" sftp -R 128 -oPort="$WPE_PORT" -oBatchMode=no -oStrictHostKeyChecking=no -r "$WPE_USERNAME@$WPE_HOSTNAME:$1"
+sshpass -p "$WPE_PASSWORD" sftp -R 128 -oPort="$WPE_PORT" -oBatchMode=no -oStrictHostKeyChecking=no -r "$WPE_USERNAME@$WPE_HOSTNAME:$1" /downloads
